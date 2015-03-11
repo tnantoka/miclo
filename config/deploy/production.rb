87bbcc8da9a24fb6a470598e647ddd9d7dbc3509
tnantoka <tnantoka@bornneet.com>
2015-03-11 13:46:12 +0900

    Fix deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,6 +59,6 @@ set :default_env, {
   github_api_key: node['miclo_cookbook']['github_api_key'],
   github_api_secret: node['miclo_cookbook']['github_api_secret'],
 
-  sendgrid_password: node['miclo_cookbook']['sendgrid_username'],
+  sendgrid_username: node['miclo_cookbook']['sendgrid_username'],
   sendgrid_password: node['miclo_cookbook']['sendgrid_password'],
 }
